b845de7fec2bb0a909d84cbca3f2e77ceb072a03
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-27 00:14:17 +0100

    Create helloworld branch

---
+++ config/application.config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'modules' => require __DIR__ . '/modules.config.php',
+    'module_listener_options' => [
+        'module_paths' => [
+            './module',
+            './vendor',
+            'PhpBenchmarksZend\\HelloWorld' => './vendor/phpbenchmarks/zend/HelloWorld/src'
+        ],
+        'config_glob_paths' => [
+            realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php',
+        ],
+        'config_cache_enabled' => true,
+        'config_cache_key' => 'application.config.cache',
+        'module_map_cache_enabled' => true,
+        'module_map_cache_key' => 'application.module.cache',
+        'cache_dir' => 'data/cache/',
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Zend\Mvc\Application;
 chdir(dirname(__DIR__));
 
 include __DIR__ . '/../vendor/autoload.php';
-$appConfig = require __DIR__ . '/../config/application.helloworld.config.php';
+$appConfig = require __DIR__ . '/../config/application.config.php';
 Application::init($appConfig)->run();
 
 // require phpbenchmarks stats.php here when needed
