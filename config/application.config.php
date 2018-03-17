67318a98a4641f6ea62c8723834ecb6e0ed6a34b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-03-18 03:36:15 +0700

    supply 'use_zend_loader' => false
    if `./vendor/phpbenchmarks/zend-framework/src` loaded by composer, it should be enough with ` 'use_zend_loader' => false` config, refs https://zendframework.github.io/zend-modulemanager/module-autoloader/#disabling-the-moduleautoloader

--- config/application.config.php
+++ config/application.config.php
@@ -3,11 +3,7 @@
 return [
     'modules' => require __DIR__ . '/modules.config.php',
     'module_listener_options' => [
-        'module_paths' => [
-            './module',
-            './vendor',
-            'PhpBenchmarksZendFramework\\HelloWorldModule' => './vendor/phpbenchmarks/zend-framework/src'
-        ],
+        'use_zend_loader' => false,
         'config_glob_paths' => [
             realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php',
         ],
